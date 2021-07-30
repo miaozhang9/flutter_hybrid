11665d180e4eec100b8f2c032dafacd8e88da743
zhangmiao <miao1.zhang@autostreets.com>
2021-07-30 10:16:09 +0800

    update

---
+++ FlutterHybridiOS/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/miaoz/.fvm/versions/1.17.1-stable"
+export "FLUTTER_APPLICATION_PATH=/Users/miaoz/Desktop/GitLab/Other/Flutter相关/flutter_boost/example_swift"
+export "FLUTTER_TARGET=/Users/miaoz/Desktop/GitLab/Other/Flutter相关/flutter_boost/example_swift/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/miaoz/.fvm/versions/1.17.1-stable/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
